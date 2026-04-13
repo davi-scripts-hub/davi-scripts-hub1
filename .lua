1a54bbc2c30bc57d56b8678fd6881ea185ff4e4f
Davi Scripts <daviscripts07@gmail.com>
2026-04-13 20:11:21 -0300

    Update script to load from new .lua file

--- .lua
+++ .lua
@@ -1,3 +1,3 @@
 script_key = "KEY"; -- A key might be required, if not, delete this line.
-loadstring(game:HttpGet("https://raw.githubusercontent.com/davi-scripts-hub/davi-scripts-hub1/refs/heads/main/.gitignore"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davi-scripts-hub/davi-scripts-hub1/refs/heads/main/.lua"))()
  
